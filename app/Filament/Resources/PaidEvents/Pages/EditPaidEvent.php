8634fed81d6231e6be535ff72674d9592438905c
Sourov Biswas <sourovcodes@gmail.com>
2025-11-14 20:15:33 +0600

    feat: add Manage Registrations action to EditPaidEvent page

--- app/Filament/Resources/PaidEvents/Pages/EditPaidEvent.php
+++ app/Filament/Resources/PaidEvents/Pages/EditPaidEvent.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\PaidEvents\Pages;
 
 use App\Filament\Resources\PaidEvents\PaidEventResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
@@ -13,6 +14,10 @@ class EditPaidEvent extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('registrations')
+                ->label('Manage Registrations')
+                ->icon('heroicon-o-clipboard')
+                ->url(fn () => PaidEventResource::getUrl('registrations', ['record' => $this->record])),
             DeleteAction::make(),
         ];
     }
